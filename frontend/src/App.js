89c1f79ec2ec61853febe08ca6b7dae9f99bf90b
risshh18 <rishabhtripathirt81@gmail.com>
2024-07-15 10:26:59 +0530

    Codebase

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="Navbar">
+      <ul className="Nav-items">
+        <l1 >
+          <a className="nav-links" href="google.com">Home</a>
+        </l1>
+        <li>
+          <a className="nav-links" href="google.com">Profile</a>
+        </li>
+        <li>
+          <a className="nav-links" href="google.com">Login/Signup</a>
+        </li>
+      </ul>
+    </div>
+    </>
+  );
+}
+
+export default App;
